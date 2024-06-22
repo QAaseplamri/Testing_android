9f2a4b6536b55ca0fc083b9749156f387b0774e7
aseplamri <aseplamri12@gmail.com>
2024-06-22 21:30:49 +0700

    g

---
+++ Test Suites/Level Up/Test Mobile.ts
@@ -0,0 +1,14 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<TestSuiteEntity>
+   <description></description>
+   <name>Test Mobile</name>
+   <tag></tag>
+   <isRerun>false</isRerun>
+   <mailRecipient></mailRecipient>
+   <numberOfRerun>3</numberOfRerun>
+   <pageLoadTimeout>30</pageLoadTimeout>
+   <pageLoadTimeoutDefault>true</pageLoadTimeoutDefault>
+   <rerunFailedTestCasesOnly>false</rerunFailedTestCasesOnly>
+   <rerunImmediately>true</rerunImmediately>
+   <testSuiteGuid>9e459607-4b98-46ce-bb85-b5814c8b260c</testSuiteGuid>
+</TestSuiteEntity>
